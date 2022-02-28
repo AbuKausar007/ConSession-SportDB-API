6ca301494c90fefdc9dc86c8ca0fdc14b5ef9e4f
AbuKausar007 <a.25kausar@gmail.com>
2022-02-28 19:53:35 +0600

    API faced

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-alert('adde');
+const allPlayers = () =>{
+    const searchText = document.getElementById('search-box').value;
+    const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchText}`
+    fetch(url)
+    .then( res => res.json())
+    .then( data => console.log(data.player));
+    console.log(url);
+}
